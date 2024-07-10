9bd1b77225e6c75bec2f1ba614c6811a7e0794eb
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-07-10 16:01:24 +0530

    added collision detection

--- flap.py
+++ flap.py
@@ -23,7 +23,6 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 
 class Pipe:
-    # provide an additonal paramter isTop
     def __init__(self,x):
         self.x = x
         self.width = 75
@@ -31,23 +30,30 @@ class Pipe:
         self.pipeGap = random.randint(70, 150)
         self.topHeight = random.randint(50, DISPLAY_HEIGHT - self.pipeGap)
         self.bottomHeight = DISPLAY_HEIGHT - self.topHeight;
+        self.bottomY = self.topHeight + self.pipeGap
 
 
     def draw(self):
+        # top pipe
         pg.draw.rect(DISPLAY, WHITE, (self.x, 0, self.width, self.topHeight))
-        pg.draw.rect(DISPLAY, WHITE, (self.x, self.topHeight+self.pipeGap, self.width, self.bottomHeight))
+        # bottom pipe
+        pg.draw.rect(DISPLAY, WHITE, (self.x, self.bottomY, self.width, self.bottomHeight))
 
     def move(self):
         self.x -= 5;
 
 class Bird:
     def __init__(self):
+        self.x = 100 # fixed
         self.y= DISPLAY_HEIGHT//2
+        self.radius = 15
         self.action = 0
-        self.start_time = round((time.time()),2)
+        self.color = WHITE
+        self.start_time = (time.time())
 
     def draw(self):
-        pg.draw.circle(DISPLAY, WHITE, (100,self.y), 15)
+        pg.draw.circle(DISPLAY, self.color, (self.x,self.y), self.radius)
+        # pg.draw.rect(DISPLAY, GREEN, (100-15, self.y, 15, 15))
         self.jump()
 
     def move(self):
@@ -63,6 +69,49 @@ class Bird:
             t = time.time()- self.start_time
             self.y += 1+ 4*t + 0.5*t**2
 
+class Collision:
+    def __init__(self, Bird, Pipe):
+        self.bird = Bird;
+        self.pipe = Pipe;
+
+        collided = (self.checkCollison())
+        if (collided):
+            self.bird.color = RED
+
+
+    def nearest(self):
+        # nearest y point
+        if self.bird.y > self.pipe.topHeight and self.bird.y < self.pipe.bottomY:
+            if(self.bird.y - self.pipe.topHeight > self.pipe.bottomY - self.bird.y):
+                nearestY = self.pipe.bottomY
+            else:
+                nearestY = self.pipe.topHeight
+        else:
+            nearestY = self.bird.y
+
+        # nearest x point
+
+        if self.bird.x < self.pipe.x:
+            nearestX = self.pipe.x
+        elif self.bird.x > self.pipe.x + self.pipe.width:
+            nearestX = self.pipe.x + self.pipe.width
+        else:
+            nearestX = self.bird.x
+
+        return (nearestX, nearestY)
+
+    def checkCollison(self):
+        pointX, pointY = self.nearest()
+
+        if ((self.bird.x - pointX)**2 + (self.bird.y - pointY)**2)**0.5 < self.bird.radius:
+            return True
+        else:
+            return False
+
+
+
+
+
 
 
 # Game loop
@@ -76,12 +125,16 @@ def main():
 
         DISPLAY.fill(BLACK)
 
+
+
+        for pipe in pipes:
+            collided = Collision(birdie, pipe)
+            pipe.draw()
+            pipe.move()
+
         # do stuff
         birdie.draw()
 
-        for pipe in pipes:
-            pipe.draw()
-            pipe.move()
         # remove out of screen pipes
         pipes = [pipe for pipe in pipes if pipe.x > -pipe.width]
         # add pipes
